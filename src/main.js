c2629e37dcd3bd0d6f6ecb7205d91da344bec161
Florent Heyraud <florentheyraud.pro@gmail.com>
2020-06-21 00:47:34 +0200

    feat(project): first commit)

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+// Entry point
+// Nouvelle instance de Vue dispo liée à la div app d'index.html
 new Vue({
   render: h => h(App),
 }).$mount('#app')
